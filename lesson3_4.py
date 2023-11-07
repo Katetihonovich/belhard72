a9bcfef5d3da677ea397d47250e50513c1f25f04
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-07 21:02:59 +0100

    add correct solution

--- lesson3_4.py
+++ lesson3_4.py
@@ -1,8 +1,15 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
 
-input_string = input("Please enter any numbers: ")
-numbers_list = input_string.split("," or " ")
-pos = [num for num in numbers_list if float(num) > 0]
-pos_count = len(pos)
-print("Positive numbers in the list: ", pos_count)
-print("Negative numbers in the list: ", len(numbers_list) - pos_count)
+# input_string = input("Please enter any numbers: ")
+# numbers_list = input_string.split("," or " ")
+# pos = [num for num in numbers_list if float(num) > 0]
+# pos_count = len(pos)
+# print("Positive numbers in the list: ", pos_count)
+# print("Negative numbers in the list: ", len(numbers_list) - pos_count)
+
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+c = int(input("Введите третье число: "))
+print("Number of positives:", (a > 0) + (b > 0) + (c > 0))
+print("Number of negatives:", (a < 0) + (b < 0) + (c < 0))

6ba40a638cb4167d1d8ab7a4ccff63dc89e92013
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-12 13:12:07 +0100

    solution lesson 4 task 3

---
+++ lesson4_3.py
@@ -0,0 +1,9 @@
+# *Заполнить словарь где ключами будут выступать числа от 0 до n, а
+# значениями вложенный словарь с ключами "name" и "email", а значения
+# для этих ключей будут браться с клавиатуры
+
+
+n = int(input("enter any number :"))
+# data = {i: {"name": input("enter your name:"), "email": input("enter your email:")} for i in range(0, n+1)}
+# print(data)
+

e9ef90a67d716ff24c755d44171e28b5e1169aac
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-15 09:05:15 +0100

    lesson 5 task 2

---
+++ lesson5_2.py
@@ -0,0 +1,26 @@
+# Сделать калькулятор: у пользователя
+# спрашивается число, потом действие и второе
+# число
+
+a, b, c = int(input("enter number:")), input("enter operation:"), int(input("enter second number:"))
+if b == "+":
+    print(a+c)
+if b == "-":
+    print(a-c)
+if b == "*":
+    print(a*c)
+if b == "/":
+    print(a/c)
+
+
+
+
+
+
+
+
+
+
+
+
+

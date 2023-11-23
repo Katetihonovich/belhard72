f77b700172348bc5bd381567562195a6defd8004
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-23 15:24:17 +0100

    correct to isinstance

--- lesson6_4.py
+++ lesson6_4.py
@@ -3,7 +3,13 @@
 # незаконно
 
 list_of = ["hello", 5, "minsk", 6, 1, 3, 2, "567", 324]
-result = filter(lambda x: isinstance(x, str), list_of)
-print(*result)
+# result = filter(lambda x: isinstance(x, str), list_of)
+# print(*result)
 
 
+def sorted_list(a):
+    return isinstance(a, str)
+
+
+new_list = filter(sorted_list, list_of)
+print("строковый список: ", *new_list)

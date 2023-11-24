c89902ed35c1ac2c1b47f51e521902b2e457c570
Ekaterina Tihonovich <katetihonovich@gmail.com>
2023-11-24 14:41:15 +0100

    solution lesson 6 task 8

---
+++ lesson6_8.py
@@ -0,0 +1,22 @@
+# Дан словарь, ключ - Название страны, значение - список городов, на вход
+# поступает город, необходимо сказать из какой он страны
+
+world = {
+    "Belarus": ["Brest", "Minsk", "Gomel", "Mogilev"],
+    "Poland": ["Warsaw", "Gdansk", "Krakow", "Lodz"],
+    "RSA": ["Johannesburg", "Pretoria", "CapeTown"]
+}
+
+
+city = input()
+def find_country():
+    for country, cities in world.items():
+        if city in cities:
+            print(f'INFO: {city} is in {country}')
+            break
+    else:
+        print(f'ERROR: Country for {city} not found')
+
+find_country()
+
+
